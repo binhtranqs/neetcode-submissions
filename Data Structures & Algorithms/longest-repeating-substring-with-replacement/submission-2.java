d13b4e4d4f8b62ec07cb052920f5612c28b9db3c
binhtranqs <tvtbqueson1112005@gmail.com>
2026-05-28 23:12:59 +0700

    Update: longest-repeating-substring-with-replacement - submission-2

--- Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.java
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.java
@@ -1,17 +1,25 @@
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        HashSet<Character> hs = new HashSet<>();
-        int maxlen=0;
-        int left=0;
-        for(int right=0;right<s.length();right++){
+    public int characterReplacement(String s, int k) {
+        int[] count = new int[26];
+
+        int left = 0;
+        int maxFreq = 0;
+        int result = 0;
+
+        for (int right = 0; right < s.length(); right++) {
             char c = s.charAt(right);
-            while(hs.contains(c)){
-                hs.remove(s.charAt(left));
+            count[c - 'A']++;
+
+            maxFreq = Math.max(maxFreq, count[c - 'A']);
+
+            while ((right - left + 1) - maxFreq > k) {
+                count[s.charAt(left) - 'A']--;
                 left++;
             }
-            hs.add(c);
-            maxlen = Math.max(maxlen, right-left +1);
+
+            result = Math.max(result, right - left + 1);
         }
-        return maxlen;
+
+        return result;
     }
 }
